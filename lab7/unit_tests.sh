165459852ef55baf24c5fa5c925b3c75e7c966d7
dukesook <devonsookhoo14@gmail.com>
2025-10-29 22:13:07 -0600

    lab7: unit tests

--- lab7/unit_tests.sh
+++ lab7/unit_tests.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 SUCCESS=1
+COUNT=0
 
 # Good Tests
 for f in tests/good/*; do
   ./lab7 < "$f" > /dev/null
   if [ $? -eq 0 ]; then
+    let COUNT+=1
     : # echo "Good Test: Passed $f"
   else
     echo "Good Test Failed: $f"
@@ -17,15 +19,16 @@ done
 for f in tests/bad/*; do
   ./lab7 < "$f" > /dev/null
   if [ $? -eq 1 ]; then
+    let COUNT+=1
     : # echo "Bad Test Passed"
   else
     echo "Bad Test Failed: $f"
     SUCCESS=0
   fi
-
-  if [ $SUCCESS -eq 1 ]; then
-    echo "All Tests Passed"
-  else
-    echo "Tests Failed!!!!!!!!!"
-  fi
 done
+
+if [ $SUCCESS -eq 1 ]; then
+  echo "All $COUNT Tests Passed"
+else
+  echo "Tests Failed!!!!!!!!!"
+fi
