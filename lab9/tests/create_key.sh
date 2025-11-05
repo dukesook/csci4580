de8f669e937f8925bc34b1e8605d9ccf83984855
dukesook <devonsookhoo14@gmail.com>
2025-11-04 17:06:14 -0700

    create_key.sh

--- lab9/tests/create_key.sh
+++ lab9/tests/create_key.sh
@@ -2,12 +2,20 @@
 clear
 clear
 
-EXE=~scooper/lab9
-EXE="../lab9"
+mkdir -p key
+
+EXE='~scooper/lab9'
+#EXE="../lab9"
 
 for filepath in good/*.al;
 do
   FILE=$(basename "${filepath%.al}")
-  echo $FILE
-  "$EXE" -o "${FILE}"  < $"filepath"
+  ~scooper/lab9 -o "key/${FILE}"  < "$filepath"
+  EXIT_CODE=$?
+  if [ $EXIT_CODE -ne 0 ]; then
+    echo "Failed to create key for ${FILE} with exit code ${EXIT_CODE}"
+  else
+    echo "Created key for ${FILE}"
+  fi
+
 done
---
+++ lab9/tests/key/t1.asm
@@ -0,0 +1,26 @@
+#  Compilers MIPS code 
+.data   
+
+
+.align 2 
+
+
+.text  
+
+.globl main 
+
+sum:			# START of FUNCION
+
+	subu $a0, $sp, 16		# # adjust the stack for function setup
+	sw $sp, ($a0)		# remember old SP
+	sw $ra, 4($a0)		# remember current Return address
+	move $sp, $a0		# # adjust the stack pointer
+
+
+	sw $t0, 8($sp)		# Load temp variable int formal paramter
+	sw $t1, 12($sp)		# Load temp variable int formal paramter
+	li $a0, 0		# RETURN has no specified value set to 0
+	lw $ra 4($sp)		# restore old environment RA
+	lw $sp ($sp)		# Return from function store SP
+
+	jr $ra			# return to the caller
