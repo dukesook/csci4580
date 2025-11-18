054580c9aa52566af4d3579a6ade9226784be0c5
dukesook <devonsookhoo14@gmail.com>
2025-11-17 19:56:26 -0700

    todo: 1 + 2 - 3

--- lab9/out.asm
+++ lab9/out.asm
@@ -10,19 +10,23 @@
 # Function Declaration
 main:			# Start of function
 
-	subu $a0, $sp, 12		# adjust the stack for function setup
+	subu $a0, $sp, 16		# adjust the stack for function setup
 	sw $sp, ($a0)		# remember old SP
 	sw $ra, 4($a0)		# remember current Return address
 	move $sp, $a0		# adjust the stack pointer
 
 
-	li $a0, 57		# Expression is a constant
+	li $a0, 1		# Expression is a constant
 	sw $a0, 8($sp)		# expression store LHS temporarily
-	li $a0, 29		# Expression is a constant
+	li $a0, 2		# Expression is a constant
 	move $a1, $a0		# Move RHS into $a1
 	lw $a0, 8($sp)		# expression restore LHS from memory
-	div $a0, $a1		# Expression DIVIDE
-	mflo $a0		# EXPR DIV
+	add $a0, $a0, $a1		# Expression PLUS
+	sw $a0, 8($sp)		# expression store LHS temporarily
+	li $a0, 3		# Expression is a constant
+	move $a1, $a0		# Move RHS into $a1
+	lw $a0, 8($sp)		# expression restore LHS from memory
+	sub $a0, $a0, $a1		# Expression MINUS
 # Function Return
 	li $a0, 0		# restore RA
 	lw $ra, 4($sp)		# restore old environment RA
--- lab9/scooper.asm
+++ lab9/scooper.asm
@@ -11,19 +11,23 @@
 
 main:			# START of FUNCION
 
-	subu $a0, $sp, 12		# # adjust the stack for function setup
+	subu $a0, $sp, 16		# # adjust the stack for function setup
 	sw $sp, ($a0)		# remember old SP
 	sw $ra, 4($a0)		# remember current Return address
 	move $sp, $a0		# # adjust the stack pointer
 
 
-	li $a0, 57		# expression is a constant
+	li $a0, 1		# expression is a constant
 	sw $a0, 8($sp)		# expression store LHS temporarily
-	li $a0, 29		# expression is a constant
+	li $a0, 2		# expression is a constant
 	move $a1, $a0		# #right hand side needs to be a1
 	lw $a0, 8($sp)		# expression restore LHS from memory
-	div $a0 $a1		# #EXPR DIV
-	mflo $a0		# #EXPR DIV
+	add $a0, $a0, $a1		# #EXPR ADD
+	sw $a0, 12($sp)		# expression store LHS temporarily
+	li $a0, 3		# expression is a constant
+	move $a1, $a0		# #right hand side needs to be a1
+	lw $a0, 12($sp)		# expression restore LHS from memory
+	sub $a0, $a0, $a1		# #EXPR SUB
 	li $a0, 0		# RETURN has no specified value set to 0
 	lw $ra 4($sp)		# restore old environment RA
 	lw $sp ($sp)		# Return from function store SP
